351988015777b544dcb3b7978311283d58b2ca06
zeld1n <zeldin2105@gmail.com>
2025-04-14 21:12:46 +0200

    Dima krasavchik 3

--- back-end/e-commerce/src/main/java/com/bogaiciuc/e_commerce/persistence/repository/UserRepository.java
+++ back-end/e-commerce/src/main/java/com/bogaiciuc/e_commerce/persistence/repository/UserRepository.java
@@ -1,17 +1,21 @@
 package com.bogaiciuc.e_commerce.persistence.repository;
-
 <<<<<<< Updated upstream
 import com.bogaiciuc.e_commerce.persistence.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
+
 =======
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
 import com.bogaiciuc.e_commerce.persistence.entity.User;
+
+// This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
+// CRUD refers Create, Read, Update, Delete
+
 >>>>>>> Stashed changes
-
-
 public interface UserRepository extends JpaRepository<User, Integer> {
 
 }
