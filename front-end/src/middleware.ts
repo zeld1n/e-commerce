eae748f10b7611ff28e758dc93ed4fa250774df4
zeld1n <zeldin2105@gmail.com>
2025-06-23 20:08:58 +0200

    fix

--- front-end/src/middleware.ts
+++ front-end/src/middleware.ts
@@ -5,15 +5,10 @@ export function middleware(request: NextRequest) {
   const sessionId = request.cookies.get('session')?.value;
   const pathname = request.nextUrl.pathname;
 
-  {/* const isAdminRoute = pathname.startsWith('/adminPanel');*/}
   const isCheckoutRoute = pathname.startsWith('/checkout');
 
-  // Для /adminPanel — просто проверяем, есть ли сессия
-    {/*if (isAdminRoute && !sessionId) {
-    return NextResponse.redirect(new URL('/login', request.url));
-  }*/}
+ 
 
-  // Для /checkout — тоже просто наличие сессии
   if (isCheckoutRoute && !sessionId) {
     return NextResponse.redirect(new URL('/login', request.url));
   }
@@ -22,5 +17,5 @@ export function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/checkout/:path*',{/* /adminPanel/:path*,*/}],
+  matcher: ['/checkout/:path*'],
 };
